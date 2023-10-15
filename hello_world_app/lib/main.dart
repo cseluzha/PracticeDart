ee538e79ca86023175473ad2bd61b93cffea5980
Chalchiuhcuauhtli <ricardo6582@gmail.com>
2023-10-14 19:16:31 -0600

    feat: add a custom button and separece the code for the counter screen

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_fuctions_screen.dart';
 
 void main() {
   // The init widget - like component or screen on React Native
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blue
       ),
-      home: const CounterScreen()
+      home: const CounterFunctionsScreen()
     );
   }
 }
